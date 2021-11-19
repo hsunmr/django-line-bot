4fdf7f24e67e74c51c7bac5af33375563ca05891
Hsun <hsunmr0823@gmail.com>
2021-11-19 16:21:00 +0800

    fix static file setting

--- django_line_bot/settings.py
+++ django_line_bot/settings.py
@@ -130,8 +130,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = "/var/www/django/static/"
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/django/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
